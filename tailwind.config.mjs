2e0f0c7b65a3c0418342f8cfcaeda0c40e7f7190
Elena <eljo0001@stud.kea.dk>
2024-10-27 15:13:44 +0100

    first commit - added menu, button og side_elena_index

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "rgba(0, 9, 26, 1)",
+        pink: "rgba(235, 55, 88, 1)",
+        white: "rgba(255, 255, 255, 1)",
+        dark_pink: "rgba(223, 22, 59, 1)",
+      },
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+};
